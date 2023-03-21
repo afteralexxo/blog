a0c5da159d8484e9d790559fa033791e139b3acf
afteralexxo <itsalextwod@gmail.com>
2023-03-20 22:13:55 -0700

    add new branch head

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
 import {
-  BrowserRouter as Router, 
+  BrowserRouter as Router,
   Routes,
   Route,
-  Link,
   Outlet
 } from 'react-router-dom';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import React from 'react'
 import LandingPage from './component/LandingPage'
 import About from './component/About'
@@ -16,36 +15,50 @@ import './component/styles/hover.css'
 import './component/styles/toggler.css'
 import './component/styles/logo.css'
 import './App.css'
+import { ClimbingBoxLoader } from 'react-spinners'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // simulate 3 seconds of loading time
+  }, []);
 
   return (
     <div className='app'>
-
-    <Router>
-    <Routes>
-      <Route path="/" element={<LayOut />}>
-        <Route index element={<LandingPage />} />
-        <Route path="#about" element={<About />} />
-        <Route path="#feedback" element={<FeedBack/>} />
-        <Route path="*" element={<NoPage />} />
-      </Route>
-    </Routes>
-  </Router>
-    
+      {
+        isLoading ? (
+          <div className='loading-page'>
+            <ClimbingBoxLoader color="#1e1e1e12" />
+          </div>
+        ) : (
+          <Router>
+            <Routes>
+              <Route path="/" element={<LayOut />}>
+                <Route index element={<LandingPage />} />
+                <Route path="#about" element={<About />} />
+                <Route path="#feedback" element={<FeedBack />} />
+                <Route path="*" element={<NoPage />} />
+              </Route>
+            </Routes>
+          </Router>
+        )
+      }
     </div>
   )
 }
 
-function LayOut(){
+function LayOut() {
 
-  return(
-   <div className="layout">
+  return (
+    <div className="layout">
       <TopBar />
       <About />
       <FeedBack />
 
-   </div>
+    </div>
   )
 }
 
@@ -53,34 +66,34 @@ function TopBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-<>
-        <nav className="navbar">
-        <a href="#home" className="logo typing-effect">"Twod/"</a>
+    <>
+      <nav className="navbar">
+        <a href="#home" className="logo typing-effect">{"<Twod/>"}</a>
         <ul className={`navbar-list ${isOpen ? 'show' : 'hide'}`}>
-           <li className='hov'>
-             <a href="#home">Home</a> 
-           </li>
-           <li className='hov'>
-             <a href="#about">About Us</a> 
-           </li>
-           <li className='hov'>
-             <a href="#feedback">Contact Us</a> 
-           </li>
-         </ul>
-         <button className="navbar-toggler btn" type="button" onClick={() => setIsOpen(!isOpen)}>
-    <span className="icon">
-        <svg viewBox="0 0 175 80" width="40" height="40">
-            <rect width="80" height="15" fill="#f0f0f0" rx="10"></rect>
-            <rect y="30" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
-            <rect y="60" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
-        </svg>
-    </span>
-    <span className="text">MENU</span>
-</button>
-       </nav>
-<Outlet/>
-</>
-);
+          <li className='hov'>
+            <a href="#home">Home</a>
+          </li>
+          <li className='hov'>
+            <a href="#about">About Us</a>
+          </li>
+          <li className='hov'>
+            <a href="#feedback">Contact Us</a>
+          </li>
+        </ul>
+        <button className="navbar-toggler btn" type="button" onClick={() => setIsOpen(!isOpen)}>
+          <span className="icon">
+            <svg viewBox="0 0 175 80" width="40" height="40">
+              <rect width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+              <rect y="30" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+              <rect y="60" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
+            </svg>
+          </span>
+          <span className="text">MENU</span>
+        </button>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
 
 export default App
