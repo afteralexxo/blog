d5ca7fff81015c8323f70e609195b455c1d301e5
afteralexxo <itsalextwod@gmail.com>
2023-03-22 11:55:16 -0700

    add loader on the form

--- src/component/FeedBack.jsx
+++ src/component/FeedBack.jsx
@@ -4,7 +4,7 @@ import './styles/form.css'
 import { useState, useEffect } from 'react'
 import './styles/btn.css'
 
-function FeedBack(){
+function FeedBack() {
 
 
   const [name, setName] = useState('');
@@ -12,38 +12,42 @@ function FeedBack(){
   const [feedback, setFeedback] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(null);
   const [message, setMessage] = useState('')
+  const [loading, setLoading] = useState(false);
 
-      const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-if(name == '' || email == '' || feedback == ''){
+    if (name == '' || email == '' || feedback == '') {
 
-  setIsSubmitted(false);
+      setIsSubmitted(false);
 
-          if(name == ''){
-            setMessage('fill the name properly')
-          }if(email == ''){
-            setMessage('fill the email properly');
-            
-          }if(feedback == ''){
-            setMessage('fill the feedback properly'); 
-          }
+      if (name == '') {
+        setMessage('fill the name properly')
+      } if (email == '') {
+        setMessage('fill the email properly');
 
-    }else{
-       
-        const data = { name, email, feedback };
-        await axios.post('https://fair-gray-goat-gown.cyclic.app/setdata', data)
+      } if (feedback == '') {
+        setMessage('fill the feedback properly');
+      }
+
+    } else {
+
+      const data = { name, email, feedback };
+      setLoading(true)
+      await axios.post('https://fair-gray-goat-gown.cyclic.app/setdata', data)
         .then(res => {
           if (res.data) {
             setIsSubmitted(true);
-            setMessage(`Dear ${data.name} thank you for the feedback`) 
+            setLoading(false)
+            setMessage(`Dear ${data.name} thank you for the feedback`)
             setName('')
             setEmail('')
             setFeedback('')
             console.log('success');
           }
-          else{
-            setMessage(`${data.name} check your form and fill properly`) 
+          else {
+            setMessage(`${data.name} check your form and fill properly`)
+            setLoading(false)
             setIsSubmitted(false);
             console.log('error');
           }
@@ -51,60 +55,69 @@ if(name == '' || email == '' || feedback == ''){
         .catch(error => {
           console.error(error);
         });
-      };
-    }
-      
+    };
+  }
 
-    return(
-        <div className="wrapper">
 
-        
-<div id="feedback" className="container">
+  return (
+    <div className="wrapper">
+
+
+      <div id="feedback" className="container">
         <div className="section animated bounceInLeft">
-            <div className="brandname">
-                <h3>FeedBack</h3>
-                <ul>
-                    <li> +251 940617356</li>
-                    <li>thelastalemayehu@gmail.com</li>
-                    <li><a href="https://alexog.netlify.app">Official Website</a> </li>
-                </ul>
-            </div>
-            <div className="contact">
-                <h3>Email Me</h3>
-                <form onSubmit={handleSubmit}>
-                    <p >
-                        <label htmlor="#">Name</label>
-                        <input type="text"
-          id="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)} name="name"/>
-                    </p>
-                    <p >
-                        <label htmlor="#">Email Address</label>
-                        <input type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)} name="email"/>
-                    </p>
-                    <p className="full">
-                        <label htmlor="#">Message</label>
-                        <textarea  id="feedback"
-          value={feedback}
-          onChange={(event) => setFeedback(event.target.value)} name="message" cols="30" rows="5"></textarea>
-                    </p>
-      {isSubmitted === true && <div className="success-message full">{message}</div>}
-      {isSubmitted === false && <div className="error-message full">{message}</div>}
-      {isSubmitted === null && <div className="neutral-message full">Please submit the form.</div>}
-                    <p className="full">
-                        <button>Submit</button>
-                    </p>
-                </form>
-            </div>
+          <div className="brandname">
+            <h3>FeedBack</h3>
+            <ul>
+              <li> +251 940617356</li>
+              <li>thelastalemayehu@gmail.com</li>
+              <li><a href="https://alexog.netlify.app">Official Website</a> </li>
+            </ul>
+          </div>
+          <div className="contact">
+            <h3>Email Me</h3>
+            <form onSubmit={handleSubmit}>
+              <p >
+                <label htmlor="#">Name</label>
+                <input type="text"
+                  id="name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)} name="name" />
+              </p>
+              <p >
+                <label htmlor="#">Email Address</label>
+                <input type="email"
+                  id="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)} name="email" />
+              </p>
+              <p className="full">
+                <label htmlor="#">Message</label>
+                <textarea id="feedback"
+                  value={feedback}
+                  onChange={(event) => setFeedback(event.target.value)} name="message" cols="30" rows="5"></textarea>
+              </p>
+              {isSubmitted === true && <div className="success-message full">{message}</div>}
+              {isSubmitted === false && <div className="error-message full">{message}</div>}
+              {isSubmitted === null && <div className="neutral-message full">Please submit the form.</div>}
+              <p className="full">
+                {loading ? <div class="loader">
+                  <span>L</span>
+                  <span>O</span>
+                  <span>A</span>
+                  <span>D</span>
+                  <span>I</span>
+                  <span>N</span>
+                  <span>G</span>
+                </div> :
+                  <button>Submit</button>}
+              </p>
+            </form>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
 
-    )
+  )
 }
 
 export default FeedBack
