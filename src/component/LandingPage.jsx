35b4df460ccb5c492f69153356c9a6d89044ca4c
afteralexxo <itsalextwod@gmail.com>
2023-03-22 12:46:47 -0700

    align the code

--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import Card from './Card'
 import Alex from './../asset/alex.png'
 
-function LandingPage(){
-    return(
-      <div id="home" className="wrapper">
+function LandingPage() {
+  return (
+    <div id="home" className="wrapper">
 
-        <div className="landing-page">
+      <div className="landing-page">
         <div className="header-main">
-        <h1 className="h1  hshed " >Building the Web, 
-          One Line of Code at a Time with </h1>
-        <p className="h5">Alemayehu Mekonen</p>
-        <p className="h6 hshed">Turning Ideas into Reality</p>
+          <h1 className="h1  hshed " >Building the Web,
+            One Line of Code at a Time with </h1>
+          <p className="h5">Alemayehu Mekonen</p>
+          <p className="h6 hshed">Turning Ideas into Reality</p>
           <button type="button" className=" btn-dark">Dark</button>
         </div>
-      <div className="img-container">
-        <Card/>
+        <div className="img-container">
+          <Card />
+        </div>
       </div>
     </div>
-      </div>
-    )
+  )
 }
 
 export default LandingPage;
--- src/component/NoPage.jsx
+++ src/component/NoPage.jsx
@@ -1,9 +1,9 @@
-function NoPage(){
-    return(
+function NoPage() {
+    return (
         <div className="wrapper">
-        <div className="no-page">
-            <h1>404 NOT FOUND</h1>
-        </div>
+            <div className="no-page">
+                <h1>404 NOT FOUND</h1>
+            </div>
         </div>
     )
 }
